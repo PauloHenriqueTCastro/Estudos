92e78cb5945d3e61c4f3317845bba0555f50ea83
Paulo Tavares <tilattavares@gmail.com>
2024-08-04 14:10:22 -0300

    Conteudo sobre java

---
+++ JAVA/JAVA.md
@@ -0,0 +1,30 @@
+Como funciona o java ?
+
+JVM: Java Virtual Machine. O java, por ser uma linguagem que consegue ser utilizada em varios sistemas operacionais diferentes, utiliza uma maquina virtual para abstrair o código escrito pelo desenvolvedor
+
+JDK: Java Development Kit. É o kit de desenvolvimento do dev. É através dele que o desenvolvedor vai conseguir manipular o arquivo java, seja para compilar ou debugar. Contem o JRE e o JVM.
+
+JRE: Java Runtime environment. É o ambiente de apenas execução do java. Responsavel por armazenar informações como classes e metodos do java.
+
+Tipos primitivos:
+int idade = 10;\
+long numeroGrande = 10000000000;\
+double salarioDouble = 2000;\
+float salarioFloat = 2500;\
+char  caractere = "M";\
+byte idadeByte = 10;\
+short idadeShorte = 10;\
+boolean isTrue = true;\
+boolean isFalse = false;
+
+Atalhos:
+
+psvm: public static void main. Cria uma função genérica:
+
+public static void main(String[] args) {\
+System.out.println("Hello World!");\
+}
+
+sout: system out printlt. Cria um "console.log":\
+System.out.println("O que voce deseja imprimir no terminal");
+
---
+++ JAVA/MeuPrimeiroHelloWorld.java
@@ -0,0 +1,41 @@
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+class                                    Declaração da classe
+HelloWorld                               Nome da classe
+pusvm                                    Método
+public static                            Modificares de ascesso
+void                                     Retorno da Função
+main                                     Nome da função
+(String[] args)                          Entrada de argumentos (Tipo do argumento / Argumento)
+System.out.println("Hello World!");      Código do desenvolvedor
+{}                                       Chaves para inicio e fechamento de funções e classes
+*/
+
+int age = 1
+int age;
+
+/*
+Para declarar uma variável, utilize a esrtutura acima onde
+int                                      Tipo da variável
+age                                      Nome da variavel
+= 1                                      Declaração do valor da variável. Dependendo do tipo, talvez não seja nescesasrio declarar.
+ */
+
+System.out.println("Hello World!");
+// Código responvael por printar no console uma informação.
+
+int idade = 10;
+long numeroGrande = 10000000000;
+double salarioDouble = 2000;
+float salarioFloat = 2500;
+char  caractere = "M";
+byte idadeByte = 10;
+short idadeShorte = 10;
+boolean isTrue = true;
+boolean isFalse = false;
+// Tipos primitivos
